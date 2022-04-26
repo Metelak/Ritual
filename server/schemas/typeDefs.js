0b26b2ca0fcaacd7d3ccf2d1fc237d20e92fa428
coyote <megan.metelak@gmail.com>
2022-04-26 14:54:07 -0500

    queries and mutations for typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,79 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    activities: [Activity]
+    goals: [Goal]
+  }
+
+  type Activity {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    link: String
+  }
+
+  type Goal {
+    _id: ID
+    name: String
+    description: String
+    createdAt: String
+    challenges: [Challenge]
+    reflection: [Reflection]
+  }
+
+  type Challenge {
+    _id: ID
+    challengeText: String
+    createdAt: String
+    username: String
+  }
+
+  type Reflection {
+    activityId: String
+    reflectionText: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addActivity(
+        activityId: String!,
+        name: String!,
+        activityText: String!,
+        image: String!,
+        link: String!
+    ): Auth
+    removeActivity(
+        activityId: String!
+    ): Auth
+    addGoal(
+        goalId: String!,
+        name: String!,
+        description: String!,
+        createdAt: String!
+    ): Goal
+    removeGoal(
+        goalId: String!
+    ): Goal
+    addChallenge(goalId: ID!, challengeText: String!): Goal
+    addReflection(goalId: ID!, reflectionText: String!): Goal
+  }
+`;
+
+module.exports = typeDefs;
