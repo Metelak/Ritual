db8b508f6c4812c4c775e535cbd2941984d01e20
Mike <mikej.miller440@gmail.com>
2022-04-27 15:58:59 -0500

    added all models to index

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const Activity = require('./Activity');
 const Goal = require('./Goal');
-// Challenge and Reflection are nested inside Goal
 const User = require('./User');
+const Challenge = require('./Challenge');
+const Reflection = require('./Reflection');
 
-module.exports = { Activity, Goal, User };
+module.exports = { Activity, Goal, User, Challenge, Reflection };
