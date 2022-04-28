74b2763c7d43ba1c96b0f3c5d2cb38aa2be007df
coyote <megan.metelak@gmail.com>
2022-04-28 17:33:26 -0500

    logo to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import logoImage from '../src/assets/Ritual_logos/Ritual_logo_grey_500x500.png';
 
 function App() {
   return (
     <div className="App">
       <h1>Ritual App under construction</h1>
+      <img src={logoImage}></img>
       <h3>From the CRMMMM developers</h3>
     </div>
   );
