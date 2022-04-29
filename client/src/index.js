1e57055331245f969b15ae2743eea60feb27b0c8
coyote <megan.metelak@gmail.com>
2022-04-29 16:18:54 -0500

    update chakra in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
