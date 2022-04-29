1959572feef5041337653ee7c9596faf5501592e
coyote <megan.metelak@gmail.com>
2022-04-28 19:09:13 -0500

    jsx for header with title, dashboard, login, signup

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,6 +5,21 @@ import Auth from '../../utils/auth';
 
 const Header = () => {
    
+  return (
+    <header>
+      <div>
+        <Link to="/">
+          <h1>Ritual</h1>
+        </Link>
+
+        <nav>
+          <Link to="/dashboard">My Rituals</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
+      </div>
+    </header>
+  )
   };
   
   export default Header;
