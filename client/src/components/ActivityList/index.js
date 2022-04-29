5606cd591f250cb1459f300d2714311eb06e353c
Mike <mikej.miller440@gmail.com>
2022-04-29 17:51:08 -0500

    created a box for activities on the home page and a box for activites on the dashboard (circle)

--- client/src/components/ActivityList/index.js
+++ client/src/components/ActivityList/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Box, Button } from '@chakra-ui/react';
 
-const ActivityHome = (props) => {
-  return <></>;
+const ActivityHome = ({ activity }) => {
+  const { title, image, text } = activity;
+
+  return (
+    <Box margin={10}>
+      <Box key={title} border="2px" p={3} m={2} borderRadius="md">
+        <div>{title}</div>
+        <div>{image}</div>
+        <div>{text}</div>
+        <Button>add to my activities</Button>
+      </Box>
+    </Box>
+  );
 };
 
-const ActivityCircle = (props) => {
-  return <></>;
+const ActivityCircle = ({ activity }) => {
+  const { title, image, text } = activity;
+  return (
+    <Box margin={10}>
+      <Box
+        key={title}
+        borderRadius="full"
+        border="2px"
+        width="200px"
+        bgImg={require(`../../assets/Ritual_logos/${image}`)}
+        height="200px">
+        <h1>{title}</h1>
+        <div>{text}</div>
+      </Box>
+    </Box>
+  );
 };
 
 export { ActivityHome, ActivityCircle };
