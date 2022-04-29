8e22ba6f187146913381fa3964c9bb1aa0f79ccd
coyote <megan.metelak@gmail.com>
2022-04-28 19:51:27 -0500

    imports added to GoalForm

--- client/src/components/GoalForm/index.js
+++ client/src/components/GoalForm/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
 
+import { useMutation } from '@apollo/client';
+import { ADD_GOAL } from '../../utils/mutations';
+import { QUERY_GOAL, QUERY_ME } from '../../utils/queries';
 
 const GoalForm = () => {
    
