96a332c58c52645e01ab3c9edcb958e8a4589f3b
Mike <mikej.miller440@gmail.com>
2022-04-29 17:50:39 -0500

    dummy data and .map for all activites

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { ActivityHome } from '../components/ActivityList';
 
-const sampleActivity = {
-  title: 'Title',
-  text: 'Text',
-  image: 'image'
-};
+const sampleActivities = [
+  {
+    title: 'Walk Outside',
+    text: 'Go for a walk outside.',
+    image: 'lotus-logo-white.png'
+  },
+  {
+    title: 'Title2',
+    text: 'Text2',
+    image: 'lotus-logo-white.png'
+  }
+];
 
-const Home = () => {};
+const Home = () => {
+  return (
+    <div>
+      {sampleActivities.map((activity) => {
+        return <ActivityHome activity={activity}></ActivityHome>;
+      })}
+    </div>
+  );
+};
 
 export default Home;
