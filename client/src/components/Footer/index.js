dcafab9980c3cd0ffb08db22ae6c71aa157d3a64
coyote <megan.metelak@gmail.com>
2022-04-30 14:03:21 -0500

    import chakra and wrapped footer with box and text

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 
 const Footer = () => {
-    return (
-        <footer>
-            <div>
-            From the CRMMMM developers &copy;{new Date().getFullYear()}
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div>
+        <Flex minWidth="max-content" alignItems="center" gap="2" bg="#2C7A7B">
+          <Box p="3">
+              <Text fontSize='xl' color='#FFFFFF'>
+              From the CRMMMM developers &copy;{new Date().getFullYear()}
+              </Text>
+          </Box>
+        </Flex>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -22,19 +22,20 @@ const Header = () => {
       <div>
         <Flex minWidth="max-content" alignItems="center" gap="2" bg='#2C7A7B'>
           <Image
+            pl='3'
             width= '100px'
             height= '75px'
             objectFit="cover"
             src={require('../../assets/Ritual_logos/lotus-logo-white.png')}
             alt="lotus logo"
           />
-          <Box p="2">
+          <Box p="3">
             <Heading as="h1" size="4xl" isTruncated color='#FFFFFF'>
               Ritual
             </Heading>
           </Box>
           <Spacer />
-          <ButtonGroup gap="2">
+          <ButtonGroup gap="2" pr="3">
             <Button colorScheme='whiteAlpha' variant='outline'>Sign Up</Button>
             <Button colorScheme='whiteAlpha' variant='outline'>Log in</Button>
           </ButtonGroup>
