266d6c40eac4efc98b1d28b50e4fd15b9d8f926e
coyote <megan.metelak@gmail.com>
2022-04-30 15:42:44 -0500

    creating a form to input new goal, importing chakra, comment out all js just to play around

--- client/src/components/GoalForm/index.js
+++ client/src/components/GoalForm/index.js
@@ -1,72 +1,17 @@
 import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 
-import { useMutation } from '@apollo/client';
-import { ADD_GOAL } from '../../utils/mutations';
-import { QUERY_ME, QUERY_USER_GOALS } from '../../utils/queries';
+// import { useMutation } from '@apollo/client';
+// import { ADD_GOAL } from '../../utils/mutations';
+// import { QUERY_ME, QUERY_USER_GOALS } from '../../utils/queries';
+
+import { FormControl } from '@chakra-ui/react';
 
 const GoalForm = () => {
-  const [description, setText] = useState('');
-  const [characterCount, setCharacterCount] = useState(0);
-
-  const [addGoal, { error }] = useMutation(ADD_GOAL, {
-    update(cache, { data: { addGoal } }) {
-      try {
-        // update goal array's cache
-        // could potentially not exist yet, so wrap in a try/catch
-        const { goals } = cache.readQuery({ query: QUERY_USER_GOALS });
-        cache.writeQuery({
-          query: QUERY_USER_GOALS,
-          data: { goals: [addGoal, ...goals] },
-        });
-      } catch (e) {
-        console.error(e);
-      }
-
-      // update me object's cache
-      const { me } = cache.readQuery({ query: QUERY_ME });
-      cache.writeQuery({
-        query: QUERY_ME,
-        data: { me: { ...me, goals: [...me.goals, addGoal] } },
-      });
-    },
-  });
-
-  // update state based on form input changes
-  const handleChange = (event) => {
-    if (event.target.value.length <= 280) {
-      setText(event.target.value);
-      setCharacterCount(event.target.value.length);
-    }
-  };
-
-  // submit form for adding new goal
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      await addGoal({
-        variables: { name, description, createdAt },
-      });
-
-      // clear form value
-      setText('');
-      setCharacterCount(0);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   return (
-    <div>
-      <p
-        className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
-      >
-        Character Count: {characterCount}/280
-        {error && <span>Something went wrong...</span>}
-      </p>
+    <FormControl isRequired>
       <form
-        onSubmit={handleFormSubmit}
+        
       >
         <textarea
           placeholder="Goal Name Here"
@@ -82,8 +27,68 @@ const GoalForm = () => {
           Create Goal
         </button>
       </form>
-    </div>
+    </FormControl>
   );
   };
   
   export default GoalForm;
+
+  // const [description, setText] = useState('');
+  // const [characterCount, setCharacterCount] = useState(0);
+
+  // const [addGoal, { error }] = useMutation(ADD_GOAL, {
+  //   update(cache, { data: { addGoal } }) {
+  //     try {
+  //       // update goal array's cache
+  //       // could potentially not exist yet, so wrap in a try/catch
+  //       const { goals } = cache.readQuery({ query: QUERY_USER_GOALS });
+  //       cache.writeQuery({
+  //         query: QUERY_USER_GOALS,
+  //         data: { goals: [addGoal, ...goals] },
+  //       });
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+
+  //     // update me object's cache
+  //     const { me } = cache.readQuery({ query: QUERY_ME });
+  //     cache.writeQuery({
+  //       query: QUERY_ME,
+  //       data: { me: { ...me, goals: [...me.goals, addGoal] } },
+  //     });
+  //   },
+  // });
+
+  // // update state based on form input changes
+  // const handleChange = (event) => {
+  //   if (event.target.value.length <= 280) {
+  //     setText(event.target.value);
+  //     setCharacterCount(event.target.value.length);
+  //   }
+  // };
+
+  // // submit form for adding new goal
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+
+  //   try {
+  //     await addGoal({
+  //       variables: { name, description, createdAt },
+  //     });
+
+  //     // clear form value
+  //     setText('');
+  //     setCharacterCount(0);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+
+  // <p
+  //       className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
+  //     >
+  //       Character Count: {characterCount}/280
+  //       {error && <span>Something went wrong...</span>}
+  //     </p>
+
+  // onSubmit={handleFormSubmit}
