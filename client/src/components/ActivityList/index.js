8e0b7f9f62b96ac5474977a5b5c7be68f4404238
coyote <megan.metelak@gmail.com>
2022-05-01 12:57:42 -0500

    switching home and dashboard activity design, trying to wrap large repeating circles

--- client/src/components/ActivityList/index.js
+++ client/src/components/ActivityList/index.js
@@ -1,41 +1,32 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  Circle,
-  Square,
-  SimpleGrid,
-  Text
-} from '@chakra-ui/react';
+import { Box, Circle, Text } from '@chakra-ui/react';
 
 const ActivityHome = ({ activity }) => {
-  const { title, image, text } = activity;
+  const { title, image } = activity;
+
+  // text
 
   return (
-    <SimpleGrid
-      className="activities"
-      margin={10}
-      columns={3}
-      minChildWidth="200px"
-      spacing={10}
-      border="2px"
-      borderRadius="md">
-      <Box key={title}>
+    <div>
+      <Circle
+        className="activities"
+        borderRadius="full"
+        width="400px"
+        height="400px"
+        m={10}
+        bgImg={require(`../../assets/activity-images/${image}`)}>
         <Text className="activity-text" fontSize="2xl" color="#81E6D9">
           {title}
         </Text>
-        <Square
-          width="300px"
-          height="300px"
-          bgImg={require(`../../assets/activity-images/${image}`)}></Square>
-      </Box>
-      <Box>{text}</Box>
-      <Box>
-        <Button>add to my activities</Button>
-      </Box>
-    </SimpleGrid>
+      </Circle>
+    </div>
   );
 };
+//Make this a modal attached to activityHome
+/* <Box>{text}</Box>
+    <Box>
+      <Button>add to my activities</Button>
+    </Box> */
 
 const ActivityCircle = ({ activity }) => {
   const { title, image, text } = activity;
@@ -43,7 +34,6 @@ const ActivityCircle = ({ activity }) => {
     <Box margin={10}>
       <Circle
         className="activities"
-        key={title}
         borderRadius="full"
         border="2px"
         width="200px"
