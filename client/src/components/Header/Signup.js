1b9b5212b1a1b0fb9dbd5b5d8b5145b0c0038376
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-30 20:07:00 -0500

    Reintroducing useState, useMutation, Auth, and ADD_USER; added more @chakra-ui/react imports for styling Signup Modal. handleChange() and handleFormSubmit() applied inside Modal. Currently no information is saved to the DB so there is still work left to do with JS/JSX.

--- client/src/components/Header/Signup.js
+++ client/src/components/Header/Signup.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
-// import { useMutation } from '@apollo/client';
-// import Auth from '../../utils/auth';
-// import { ADD_USER } from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
+import Auth from '../../utils/auth';
+import { ADD_USER } from '../../utils/mutations';
 
-
+// Imports from @chakra-ui/react to assist with Modal and form styling.
 import {
   Button,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -14,17 +21,59 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  FormControl,
-  useDisclosure,
-  FormLabel,
-  Input
+  useDisclosure
 } from '@chakra-ui/react';
 
 function SignupForm() {
+  // Importing functions from @chakra-ui/react for when Modal isOpn, onOpen, onClose as useDisclosure
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  // initialRef is where the cursor loads upon Modal opening for the user
   const initialRef = React.useRef();
+  // finalRef is the last input field for user before submit or cancel button
   const finalRef = React.useRef();
+
+  // Setting form state by using state for already registered users with email and password fields.
+  const [formState, setFormState] = useState({ email: '', password: '' });
+
+  // Using mutation addUser to pull necessary registration fields we use in handleFormSubmit()
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  // Defining error so Modal knows on which condition to present error text to user.
+  const isError = error;
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    // The block of code to be tested for errors while the program is being executed is written in the try block
+    try {
+      const mutationResponse = await addUser({
+        variables: {
+          username: formState.username,
+          email: formState.email,
+          password: formState.password
+        }
+      });
+      // Using Auth.js imported at the top, the user's credentials are verified with their JWT
+      const token = mutationResponse.data.addUser.token;
+      Auth.login(token);
+    } catch (e) {
+      // if errors are presented, use catch to console.log(e)
+      console.log(e);
+    }
+  };
+
+  // Any time form input has been added it registers on the page as users type, generating and returning updated form state.
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
+  // Create a password input with a show/hide password functionality
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
 
   return (
     <>
@@ -42,22 +91,51 @@ function SignupForm() {
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>Username</FormLabel>
-              <Input ref={initialRef} placeholder="username" />
+              <Input
+                ref={initialRef}
+                className="username-input"
+                placeholder="username"
+                onChange={handleChange}
+              />
             </FormControl>
 
             <FormControl>
               <FormLabel>Email Address</FormLabel>
-              <Input ref={initialRef} placeholder="email" />
+              <Input
+                className="email-input"
+                placeholder="email"
+                onChange={handleChange}
+              />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Password</FormLabel>
-              <Input placeholder="password" />
+              <InputGroup>
+                <Input
+                  className="password-input"
+                  placeholder="******"
+                  name="password"
+                  type={show ? 'text' : 'password'}
+                  id="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              {!isError ? (
+                <FormHelperText> </FormHelperText>
+              ) : (
+                <FormErrorMessage>User credentials required.</FormErrorMessage>
+              )}
             </FormControl>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
+            <Button onSubmit={handleFormSubmit} colorScheme="blue" mr={3}>
               Save
             </Button>
             <Button onClick={onClose}>Cancel</Button>
@@ -66,86 +144,6 @@ function SignupForm() {
       </Modal>
     </>
   );
-}
-
-// function Signup(props) {
-//   const [formState, setFormState] = useState({ email: '', password: '' });
-//   const [addUser] = useMutation(ADD_USER);
-
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     const mutationResponse = await addUser({
-//       variables: {
-//         email: formState.email,
-//         password: formState.password,
-//         firstName: formState.firstName,
-//         lastName: formState.lastName,
-//       },
-//     });
-//     const token = mutationResponse.data.addUser.token;
-//     Auth.login(token);
-//   };
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormState({
-//       ...formState,
-//       [name]: value,
-//     });
-//   };
-
-//   return (
-//     <div className="container my-1">
-//       <Link to="/login">← Go to Login</Link>
-
-//       <h2>Signup</h2>
-//       <form onSubmit={handleFormSubmit}>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="firstName">First Name:</label>
-//           <input
-//             placeholder="First"
-//             name="firstName"
-//             type="firstName"
-//             id="firstName"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="lastName">Last Name:</label>
-//           <input
-//             placeholder="Last"
-//             name="lastName"
-//             type="lastName"
-//             id="lastName"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="email">Email:</label>
-//           <input
-//             placeholder="youremail@test.com"
-//             name="email"
-//             type="email"
-//             id="email"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="pwd">Password:</label>
-//           <input
-//             placeholder="******"
-//             name="password"
-//             type="password"
-//             id="pwd"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row flex-end">
-//           <button type="submit">Submit</button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// }
+};
 
 export default SignupForm;
