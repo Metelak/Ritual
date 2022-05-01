14628e588ac61453c23aa73019ac4d06fbf14f24
coyote <megan.metelak@gmail.com>
2022-05-01 17:02:40 -0500

    moved model into activityhome

--- client/src/components/ActivityList/index.js
+++ client/src/components/ActivityList/index.js
@@ -17,26 +17,7 @@ import {
 } from '@chakra-ui/react';
 
 const ActivityHome = ({ activity }) => {
-  const { title, image } = activity;
-
-  return (
-    <WrapItem p={10}>
-      <Circle
-        className="activities"
-        borderRadius="full"
-        width="300px"
-        height="300px"
-        bgImg={require(`../../assets/activity-images/${image}`)}>
-        <Text className="activity-text" fontSize="2xl" color="#81E6D9">
-          {title}
-        </Text>
-      </Circle>
-    </WrapItem>
-  );
-};
-
-const ActivityDescription = ({ activity }) => {
-  const { title, text } = activity;
+  const { title, image, text } = activity;
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -44,10 +25,18 @@ const ActivityDescription = ({ activity }) => {
   const finalRef = React.useRef();
 
   return (
-    <>
-      <Button onClick={onOpen}>{title}</Button>
-
-      <Modal
+    <WrapItem p={10}>
+      <Circle
+        className="activities"
+        onClick={onOpen}
+        borderRadius="full"
+        width="300px"
+        height="300px"
+        bgImg={require(`../../assets/activity-images/${image}`)}>
+        <Text className="activity-text" fontSize="2xl" color="#81E6D9">
+          {title}
+        </Text>
+        <Modal
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
         isOpen={isOpen}
@@ -64,9 +53,43 @@ const ActivityDescription = ({ activity }) => {
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </>
+      </Circle>
+    </WrapItem>
   );
 };
+
+// const ActivityDescription = ({ activity }) => {
+//   const { title, text } = activity;
+
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+
+//   const initialRef = React.useRef();
+//   const finalRef = React.useRef();
+
+//   return (
+//     <>
+//       <Button onClick={onOpen}>{title}</Button>
+
+//       <Modal
+//         initialFocusRef={initialRef}
+//         finalFocusRef={finalRef}
+//         isOpen={isOpen}
+//         onClose={onClose}>
+//         <ModalOverlay />
+//         <ModalContent>
+//           <ModalHeader>{title}</ModalHeader>
+//           <ModalCloseButton />
+//           <ModalBody>{text}</ModalBody>
+//           <ModalFooter>
+//             <Button colorScheme="teal" variant="outline">
+//               Save Activity
+//             </Button>
+//           </ModalFooter>
+//         </ModalContent>
+//       </Modal>
+//     </>
+//   );
+// };
 //Make this a modal attached to activityHome, make the {title a button for modal}
 /* <Box>{text}</Box>
     <Box>
@@ -92,4 +115,4 @@ const ActivityDash = ({ activity }) => {
   );
 };
 
-export { ActivityHome, ActivityDescription, ActivityDash };
+export { ActivityHome, ActivityDash };
