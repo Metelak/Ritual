aaaf58b06bb28be423368c0d2b07005031275f85
Mike <mikej.miller440@gmail.com>
2022-05-01 21:24:49 -0500

    got rid of fake user data

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -16,69 +16,11 @@ import GoalForm from '../components/GoalForm';
 import { ActivityDash } from '../components/ActivityList';
 import GoalList from '../components/GoalList';
 
-const fakeUserData = {
-  _id: '626dd2c8db8783636bb74a0f',
-  username: 'mike',
-  email: 'mike@mike.com',
-  activities: [
-    {
-      _id: '626dd27dcbfc48efe3a4edb9',
-      title: 'Deep Breathing',
-      text: 'nulla ultrices aliquet maecenas leo odio condimentum id luctus',
-      image: 'deep-breathing.png'
-    },
-    {
-      _id: '626dd27dcbfc48efe3a4edbd',
-      title: 'Yoga',
-      text: 'nam congue risus semper porta volutpat quam pede lobortis',
-      image: 'yoga.png'
-    }
-  ],
-  goals: [
-    {
-      _id: '626dd43bd3ee7a5ae5824836',
-      name: 'stretch!',
-      description: "make sure you're stretching. No old man back here!",
-      createdAt: 'Apr 30th, 2022 at 7:28 pm',
-      challenges: [],
-      reflection: [
-        {
-          _id: '626dd45fd3ee7a5ae582483f',
-          reflectionText: 'I did it! Woohoo!',
-          createdAt: 'Apr 30th, 2022 at 7:29 pm'
-        }
-      ],
-      isComplete: false
-    },
-    {
-      _id: '626dd44fd3ee7a5ae5824839',
-      name: 'Smile',
-      description: 'Happy thoughts',
-      createdAt: 'Apr 30th, 2022 at 7:29 pm',
-      challenges: [
-        {
-          _id: '626dd474d3ee7a5ae5824844',
-          challengeText: 'This is challenging!',
-          createdAt: 'Apr 30th, 2022 at 7:29 pm'
-        }
-      ],
-      reflection: [
-        {
-          _id: '626dd48ad3ee7a5ae5824846',
-          reflectionText: 'This was fun to think about',
-          createdAt: 'Apr 30th, 2022 at 7:30 pm'
-        }
-      ],
-      isComplete: false
-    }
-  ]
-};
-
 const Dashboard = () => {
   // query me
   const { loading, error, data: userData } = useQuery(QUERY_ME);
 
-  const user = userData?.me || {};
+  const user = userData?.me;
 
   if (error) {
     return (
@@ -127,7 +69,9 @@ const Dashboard = () => {
           </Box>
         </Box>
         <Box borderWidth="2px" w="50%" borderRadius="lg">
-          <Center>My Goals</Center>
+          <Heading className="center-text" fontSize="3xl" color="#2C7A7B">
+            My Goals
+          </Heading>
           {user.goals.map((goal) => {
             return <GoalList key={goal._id} goal={goal} />;
           })}
