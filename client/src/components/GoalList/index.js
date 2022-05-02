fdd65fa2ec9b581ab1efa5ebdd6a7b1f2ad0a5e4
Mike <mikej.miller440@gmail.com>
2022-05-02 10:39:29 -0500

    added new components and styled

--- client/src/components/GoalList/index.js
+++ client/src/components/GoalList/index.js
@@ -1,11 +1,26 @@
-import { Box, ButtonGroup, Center, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Text,
+  useDisclosure,
+  Divider,
+  Flex
+} from '@chakra-ui/react';
+import { ArrowDownIcon } from '@chakra-ui/icons';
 import React from 'react';
 import { ChallengeForm } from '../ChallengeForm';
 import { ReflectionForm } from '../ReflectionForm';
+import GoalChallenges from './challenges';
+import GoalReflection from './reflection';
 
 const GoalList = ({ goal }) => {
   // destructure props
   const { _id, name, description, createdAt, challenges, reflection } = goal;
+
+  const { isOpen: challengeOpen, onToggle: toggleChallenges } = useDisclosure();
+  const { isOpen: reflectionOpen, onToggle: toggleReflection } =
+    useDisclosure();
 
   return (
     <Box margin={10}>
@@ -13,15 +28,49 @@ const GoalList = ({ goal }) => {
         <Text fontSize="2xl">{name}</Text>
         <div>{description}</div>
         <div>created at: {createdAt}</div>
-        <div>{challenges.length} challenges</div>
-        <div>{reflection.length} reflection</div>
-        <Center>
-          <ButtonGroup size="md" isAttached variant="outline">
-            <ChallengeForm goalId={_id} />
-            <ReflectionForm goalId={_id} />
-          </ButtonGroup>
-        </Center>
+        <Flex mt="10" height="40px" flexDir="row" justifyContent="space-evenly">
+          <Box>
+            <ButtonGroup isAttached variant="outline">
+              <Button
+                onClick={toggleChallenges}
+                colorScheme="red"
+                icon={<ArrowDownIcon />}>
+                Challenges
+                {challenges.length === 0 ? '' : `(${challenges.length})`}
+              </Button>
+              <ChallengeForm goalId={_id} />
+            </ButtonGroup>
+          </Box>
+          <Divider orientation="vertical" />
+          <Box>
+            <ButtonGroup isAttached variant="outline">
+              <Button
+                onClick={toggleReflection}
+                colorScheme="teal"
+                icon={<ArrowDownIcon />}>
+                Reflection
+              </Button>
+              <ReflectionForm goalId={_id} />
+            </ButtonGroup>
+          </Box>
+        </Flex>
       </Box>
+
+      {/* Pop-down for challenges */}
+      <GoalChallenges
+        challengeOpen={challengeOpen}
+        challenges={challenges}
+        toggleChallenges={toggleChallenges}
+        goalName={name}
+      />
+
+      {/* Pop-down for reflections */}
+      <GoalReflection
+        reflectionOpen={reflectionOpen}
+        toggleReflection={toggleReflection}
+        reflections={reflection}
+        goalName={name}
+      />
     </Box>
   );
 };
