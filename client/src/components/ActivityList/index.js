045e8b184f930bc50a43efe7a4fb37fb4a46582c
coyote <megan.metelak@gmail.com>
2022-05-03 15:34:59 -0500

    connect queries and mutations to add activities to user dashboard

--- client/src/components/ActivityList/index.js
+++ client/src/components/ActivityList/index.js
@@ -16,9 +16,24 @@ import {
   WrapItem,
   SimpleGrid
 } from '@chakra-ui/react';
+import { ADD_ACTIVITY } from '../../utils/mutations';
+import { QUERY_ME } from '../utils/queries';
 
 const ActivityHome = ({ activity }) => {
   const { title, image, text } = activity;
+  // add activity mutation setup
+  const [addActivity, { error }] = useMutation(ADD_ACTIVITY);
+
+  const addActivityHandler = async () => {
+    try {
+      await addActivity({
+        variables: { id: _id }
+      });
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -48,7 +63,7 @@ const ActivityHome = ({ activity }) => {
           <ModalCloseButton />
           <ModalBody>{text}</ModalBody>
           <ModalFooter>
-            <Button colorScheme="teal" variant="outline">
+            <Button colorScheme="teal" variant="outline" onClick={addActivityHandler}>
               Save Activity
             </Button>
           </ModalFooter>
@@ -61,21 +76,38 @@ const ActivityHome = ({ activity }) => {
 
 const ActivityDash = ({ activity }) => {
   const { title, image, text } = activity;
-  return (
-    <Box margin={5}>
-      <SimpleGrid className="activities" columns={2} border="2px" borderColor='#FFFFFF' borderRadius="md" bg="teal">
-        <Square
-          width="200px"
-          height="200px"
-          bgImg={require(`../../assets/activity-images/${image}`)}>
-          <Text className="activity-text" fontSize="2xl" color="#81E6D9">
-            {title}
-          </Text>
-        </Square>
-        <Square color='#FFFFFF'>{text}</Square>
-      </SimpleGrid>
-    </Box>
-  );
-};
+
+  const addActivity = () => {
+    const { data: userData, loading } = useQuery(QUERY_ME);
+
+    const allActivities = userData?.me.activities;
+  
+    if (loading) {
+      return <div>Loading your activities...</div>;
+    }
+  
+    let savedActivities;
+    if (allActivities) {
+      savedActivities = allActivities.filter((activity) => {
+        return activity.isSaved === true;
+      });
+    }
+    return (
+      <Box margin={5}>
+        <SimpleGrid className="activities" columns={2} border="2px" borderColor='#FFFFFF' borderRadius="md" bg="teal">
+          <Square
+            width="200px"
+            height="200px"
+            bgImg={require(`../../assets/activity-images/${image}`)}>
+            <Text className="activity-text" fontSize="2xl" color="#81E6D9">
+              {title}
+            </Text>
+          </Square>
+          <Square color='#FFFFFF'>{text}</Square>
+        </SimpleGrid>
+      </Box>
+    );
+  }
+}
 
 export { ActivityHome, ActivityDash };
