adb78481c66fa4f905f27119b1084f6129a72719
Mike <mikej.miller440@gmail.com>
2022-05-04 19:23:33 -0500

    set up breakpoints on dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -13,8 +13,7 @@ import {
   ButtonGroup,
   Divider,
   Stack,
-  Wrap,
-  WrapItem
+  Flex
 } from '@chakra-ui/react';
 import { useMutation, useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
@@ -108,115 +107,118 @@ const Dashboard = () => {
   }
 
   return (
-    <Wrap>
-      <Stack direction={['column', 'row']}>
-        <WrapItem>
-      <Box w="50%" minHeight="100%" bg="#FFFFFF">
-          <Heading
-            className="center-text"
-            fontSize="3xl"
-            color="#2C7A7B"
-            mt="20px">
-            My Activities
-          </Heading>
-          {userActivities.length === 0 ? (
-            <ScaleFade in>
-              <Box m="30px">
-                <Alert
-                  status="info"
-                  variant="subtle"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  textAlign="center"
-                  borderRadius="md"
-                  height="200px">
-                  <AlertIcon boxSize="40px" mr={0} />
-                  <AlertTitle mt={4} mb={1} fontSize="lg">
-                    No activities!
-                  </AlertTitle>
-                  <AlertDescription maxWidth="sm">
-                    Go to the{' '}
-                    <i>
-                      <Link to="/">homepage</Link>
-                    </i>{' '}
-                    to view and add activities.
-                  </AlertDescription>
-                </Alert>
-              </Box>
-            </ScaleFade>
-          ) : (
-            <Box templateColumns="repeat(5, 1fr)" gap={6}>
-              {userActivities.map((activity) => {
-                return (
-                  <ActivityDash
-                    key={activity._id}
-                    activity={activity}></ActivityDash>
-                );
-              })}
+    <Flex flexDir={['column', 'column', 'row']} alignItems="center">
+      <Box w={['80%', '80%', '50%']} minHeight="100%" bg="#FFFFFF">
+        <Heading
+          className="center-text"
+          fontSize="3xl"
+          color="#2C7A7B"
+          mt="20px">
+          My Activities
+        </Heading>
+        {userActivities.length === 0 ? (
+          <ScaleFade in>
+            <Box m="30px">
+              <Alert
+                status="info"
+                variant="subtle"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                textAlign="center"
+                borderRadius="md"
+                height="200px">
+                <AlertIcon boxSize="40px" mr={0} />
+                <AlertTitle mt={4} mb={1} fontSize="lg">
+                  No activities!
+                </AlertTitle>
+                <AlertDescription maxWidth="sm">
+                  Go to the{' '}
+                  <i>
+                    <Link to="/">homepage</Link>
+                  </i>{' '}
+                  to view and add activities.
+                </AlertDescription>
+              </Alert>
             </Box>
-          )}
-        </Box>
-        </WrapItem>
-        <Center>
-        <Divider orientation='vertical' width='1px' minHeight='100%' variant='solid' bg='#234E52' />
-        </Center>
-        <WrapItem>
-        <Box w="50%" minHeight='100%'>
-          <Heading
-            className="center-text"
-            fontSize="3xl"
-            color="#2C7A7B"
-            mt="20px">
-            My Goals
-          </Heading>
-          <Center>
-            <ButtonGroup isAttached mt="3">
-              <GoalForm />
-              <Button
-                onClick={handleViewCompletedGoals}
-                colorScheme="teal"
-                size="md">
-                Completed Goals
-              </Button>
-            </ButtonGroup>
-          </Center>
-          {userGoals.length === 0 ? (
-            <ScaleFade in>
-              <Box m="30px">
-                <Alert
-                  status="info"
-                  variant="subtle"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  textAlign="center"
-                  height="200px"
-                  borderRadius="md">
-                  <AlertIcon boxSize="40px" mr={0} />
-                  <AlertTitle mt={4} mb={1} fontSize="lg">
-                    No goals!
-                  </AlertTitle>
-                  <AlertDescription maxWidth="sm">
-                    Click Add Goal to create a goal.
-                  </AlertDescription>
-                </Alert>
-              </Box>
-            </ScaleFade>
-          ) : (
-            userGoals.map((goal) => {
+          </ScaleFade>
+        ) : (
+          <Box templateColumns="repeat(5, 1fr)" gap={6}>
+            {userActivities.map((activity) => {
               return (
-                <GoalList
-                  key={goal._id}
-                  goal={goal}
-                  completeGoal={completeGoal}
-                />
+                <ActivityDash
+                  key={activity._id}
+                  activity={activity}></ActivityDash>
               );
-            })
-          )}
-        </Box>
-        </WrapItem>
-      </Stack>
+            })}
+          </Box>
+        )}
+      </Box>
+
+      <Center>
+        <Divider
+          orientation="vertical"
+          width="1px"
+          minHeight="100%"
+          variant="solid"
+          bg="#234E52"
+        />
+      </Center>
+
+      <Box w={['80%', '80%', '50%']} minHeight="100%">
+        <Heading
+          className="center-text"
+          fontSize="3xl"
+          color="#2C7A7B"
+          mt="20px">
+          My Goals
+        </Heading>
+        <Center>
+          <ButtonGroup isAttached mt="3">
+            <GoalForm />
+            <Button
+              onClick={handleViewCompletedGoals}
+              colorScheme="teal"
+              size="md">
+              Completed Goals
+            </Button>
+          </ButtonGroup>
+        </Center>
+        {userGoals.length === 0 ? (
+          <ScaleFade in>
+            <Box m="30px">
+              <Alert
+                status="info"
+                variant="subtle"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                textAlign="center"
+                height="200px"
+                borderRadius="md">
+                <AlertIcon boxSize="40px" mr={0} />
+                <AlertTitle mt={4} mb={1} fontSize="lg">
+                  No goals!
+                </AlertTitle>
+                <AlertDescription maxWidth="sm">
+                  Click Add Goal to create a goal.
+                </AlertDescription>
+              </Alert>
+            </Box>
+          </ScaleFade>
+        ) : (
+          userGoals.map((goal) => {
+            return (
+              <GoalList
+                key={goal._id}
+                goal={goal}
+                completeGoal={completeGoal}
+              />
+            );
+          })
+        )}
+      </Box>
+    </Flex>
   );
 };
 
--- client/src/utils/theme.js
+++ client/src/utils/theme.js
@@ -1,18 +1,18 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
 
 const breakpoints = {
   sm: '30em',
   md: '48em',
   lg: '62em',
   xl: '80em',
-  '2xl': '96em',
-}
+  '2xl': '96em'
+};
 
 const theme = extendTheme({
   fonts: {
     heading: 'Truculenta, sans-serif'
   },
   breakpoints
-})
+});
 
-export default theme
+export default theme;
